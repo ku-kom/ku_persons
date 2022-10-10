a1648b257a52803f48f33d3aecd751c1d4f8f254
nanna-dk <nanna@e-nanna.dk>
2022-10-10 15:24:33 +0200

    Tables

--- ext_tables.sql
+++ ext_tables.sql
@@ -6,3 +6,12 @@ CREATE TABLE tt_content (
     ku_persons_list_search varchar(255) DEFAULT '' NOT NULL,
     ku_persons_list varchar(255) DEFAULT '' NOT NULL
 );
+
+CREATE TABLE tx_kupersons (
+    realName varchar(80) DEFAULT '' NOT NULL,
+    jobTitle varchar(255) DEFAULT '' NOT NULL,
+    email varchar(255) DEFAULT '' NOT NULL,
+    telephone VARCHAR(32) NOT NULL default NULL,
+    mobile VARCHAR(32) NOT NULL default NULL,
+    PRIMARY KEY (email)
+);
