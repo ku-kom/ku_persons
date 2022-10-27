7e8455d8373eec87e71356bf780565a2dc1b5ffe
nanna-dk <nanna@e-nanna.dk>
2022-10-27 14:27:54 +0200

    Handle error messages

--- Classes/Backend/Wizard/SuggestWizardReceiver.php
+++ Classes/Backend/Wizard/SuggestWizardReceiver.php
@@ -17,6 +17,9 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\StringUtility;
 use TYPO3\CMS\Core\Http\RequestFactory;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\CMS\Core\Messaging\FlashMessage;
+use TYPO3\CMS\Core\Messaging\FlashMessageService;
 
 class SuggestWizardReceiver extends SuggestWizardDefaultReceiver
 {
@@ -35,38 +38,68 @@ class SuggestWizardReceiver extends SuggestWizardDefaultReceiver
               'searchstring' => $query
             ]
           ];
-        $response = $requestFactory->request($url, 'POST', $additionalOptions);
-        // Get the content on a successful request
-        if ($response->getStatusCode() === 200) {
-            if (false !== strpos($response->getHeaderLine('Content-Type'), 'application/json')) {
-                $string = $response->getBody()->getContents();
-                // getContents() returns a string
-                // Convert string back to json
-                $string = iconv('ISO-8859-1', 'UTF-8', $string);
-                $data = json_decode((string) $string, true);
 
-                $items = $data['root']['employees'];
-                foreach ($items as $employee) {
-                    $newUid = StringUtility::getUniqueId('NEW');
-                    $rows[$this->table . '_' . $newUid] = [
-                        'class' => '',
-                        'label' => $params['value'],
-                        'path' => '',
-                        'sprite' => '',
-                        'style' => '',
-                        'table' => $this->table,
-                        'text' => '<table class="table-items">
+        if (!empty($url)) {
+            try {
+                $response = $requestFactory->request($url, 'POST', $additionalOptions);
+                // Get the content on a successful request
+                if ($response->getStatusCode() === 200) {
+                    if (false !== strpos($response->getHeaderLine('Content-Type'), 'application/json')) {
+                        $string = $response->getBody()->getContents();
+                        // getContents() returns a string
+                        // Convert string back to json
+                        $string = iconv('ISO-8859-1', 'UTF-8', $string);
+                        $data = json_decode((string) $string, true);
+
+                        $items = $data['root']['employees'];
+                        foreach ($items as $employee) {
+                            $newUid = StringUtility::getUniqueId('NEW');
+                            $rows[$this->table . '_' . $newUid] = [
+                                'class' => '',
+                                'label' => $params['value'],
+                                'path' => '',
+                                'sprite' => '',
+                                'style' => '',
+                                'table' => $this->table,
+                                'text' => '<table class="table-items">
                                         <tr>
                                             <td class="img-fluid img-employee"><img src="'. $employee['FOTOURL'] .'" alt="" class="list-item-img" /></td>
                                             <td><div class="employee-name">'.$employee['PERSON_FORNAVN'] . ' ' . $employee['PERSON_EFTERNAVN'] .'</div>'. $employee['ANSAT_UOFF_STIL_TEKST'] .'<br>'. $employee['ANSAT_ARB_EMAIL'] .'</td>
                                         </tr>
                                     </table>',
-                        'uid' => 67 //$employee['ANSAT_ARB_EMAIL'],
-                    ];
+                                'uid' => 67 //$employee['ANSAT_ARB_EMAIL'],
+                            ];
+                        }
+                    }
+                } else {
+                    // Sisplay error message
+                    $flashMessage = GeneralUtility::makeInstance(
+                        \TYPO3\CMS\Core\Messaging\FlashMessage::class,
+                        $this->getLanguageService()->sL('LLL:EXT:ku_persons/Resources/Private/Language/locallang.xlf:persons.error'),
+                        '',
+                        \TYPO3\CMS\Core\Messaging\FlashMessage::ERROR,
+                        true
+                    );
+                    $flashMessageService = GeneralUtility::makeInstance(FlashMessageService::class);
+                    $defaultFlashMessageQueue = $flashMessageService->getMessageQueueByIdentifier();
+                    $defaultFlashMessageQueue->enqueue($flashMessage);
                 }
+
+                return $rows;
+            } catch (\Exception $e) {
+                // Display error message
+                $this->addFlashMessage(
+                    'Error: ' . $e->getMessage(),
+                    '',
+                    FlashMessage::ERROR,
+                    false
+                );
             }
         }
+    }
 
-        return $rows;
+    protected function getLanguageService(): LanguageService
+    {
+        return $GLOBALS['LANG'];
     }
 }
